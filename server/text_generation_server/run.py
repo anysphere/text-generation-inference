b4d494a89fb0da5a40f3bb34e9e954dd7de21a54
Sanger2000 <amanrsanger@gmail.com>
2023-07-19 02:29:00 -0700

    impl

--- server/text_generation_server/run.py
+++ server/text_generation_server/run.py
@@ -156,6 +156,9 @@ def main():
         prompt_len = int(data[0].item())
         gen_size = int(data[1].item())
 
+        print(f'prompt_len: {prompt_len}')
+        print(f'gen_size: {gen_size}')
+
         for i, batch_size in enumerate([1, 2, 4]):
             print(f'batch_size: {batch_size}')
             run_experiment(
